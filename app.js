2bda0883bf52c27b8de5ee87576597f13d853e06
Drago78624 <maazahmed78624@gmail.com>
2022-08-19 10:01:34 +0500

    modal added

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("odin library");
+const modal = document.getElementById("modal");
+const modalBtn = document.getElementById("openModal");
+
+modalBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
