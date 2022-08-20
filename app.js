3e743304602d19f4ac4c09a5fe8ffc99691218ae
Drago78624 <maazahmed78624@gmail.com>
2022-08-20 21:07:22 +0500

    books creation added

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 
 const modal = document.getElementById("modal");
 const modalBtn = document.getElementById("openModal");
+const addBtn = document.getElementById("add-btn");
+const title = document.querySelector(".title")
+const author = document.querySelector(".author")
+const pages = document.querySelector(".pages")
+const readStatus = document.querySelector(".read-status")
+const form = document.getElementById("form")
+const booksContainer = document.querySelector(".books-container")
 
 modalBtn.addEventListener("click", () => {
   modal.style.display = "flex";
@@ -12,3 +19,39 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+
+let myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.readStatus = readStatus
+  this.changeReadStatus = function(){
+    this.readStatus = !this.readStatus
+  }
+}
+
+function addBookToLibrary(e) {
+  e.preventDefault();
+  let titleVal = title.value
+  let authorVal = author.value
+  let pagesVal = pages.value
+  let readStatusVal = readStatus.checked
+  if(titleVal && authorVal && pagesVal){
+    const newBook = new Book(titleVal, authorVal, pagesVal, readStatusVal)
+    myLibrary.push(newBook)
+    form.reset()
+    modal.style.display = "none";
+    showAddedBooks()
+  }
+  console.log(myLibrary);
+}
+
+function showAddedBooks(){
+  
+}
+
+
+addBtn.addEventListener("click", addBookToLibrary)
