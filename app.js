21d694786eacfcf5aa8731c9f1c6b21ef124d0b7
Drago78624 <maazahmed78624@gmail.com>
2022-08-22 12:55:44 +0500

    completed

--- app.js
+++ app.js
@@ -27,9 +27,6 @@ function Book(title, author, pages, readStatus) {
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus;
-  this.changeReadStatus = function () {
-    this.readStatus = !this.readStatus;
-  };
 }
 
 function addBookToLibrary(e) {
@@ -45,7 +42,6 @@ function addBookToLibrary(e) {
     modal.style.display = "none";
     showAddedBooks(myLibrary);
   }
-  console.log(myLibrary);
 }
 
 function showAddedBooks(library) {
@@ -53,18 +49,44 @@ function showAddedBooks(library) {
   for (let i = 0, length = library.length; i < length; i++) {
     const element = document.createElement("div")
     element.className = "book"
+    element.setAttribute("data-id", library.indexOf(library[i]));
     let html = `
           <h2 class="book-name">${library[i].title.toUpperCase()}</h2>
           <h3 class="book-author">"${library[i].author}"</h3>
           <h4 class="book-pages">${library[i].pages}</h4>
-          <button class="has-read-btn btn">${
+          <button class="has-read-btn btn" id="status-change-btn">${
             library[i].readStatus ? "has read" : "not read yet"
           }</button>
-          <button class="delete-btn btn">delete</button>
+          <button class="delete-btn btn" id="delete-btn">delete</button>
     `;
     element.innerHTML = html
     booksContainer.appendChild(element)
   }
 }
+
+document.addEventListener('click',function(e){
+  if(e.target && e.target.id == 'delete-btn'){
+        const newLibrary = myLibrary.filter((book, i)=>{
+          return i != e.target.parentElement.dataset.id
+        })
+        myLibrary = newLibrary
+        showAddedBooks(myLibrary)
+   }
+   if(e.target && e.target.id == 'status-change-btn'){
+    let id = e.target.parentElement.dataset.id
+    const newLibrary = myLibrary.map((book, i)=>{
+      if(id == i){
+        return {
+          ...book,
+          readStatus: !book.readStatus
+        }
+      }else {
+        return book
+      }
+    })
+    myLibrary = newLibrary
+    showAddedBooks(myLibrary)
+}
+});
 
 addBtn.addEventListener("click", addBookToLibrary);
